88e7674a5d79e3a49209cf7566a606f43f73be37
toky-todinirina <tokyangelo050@gmail.com>
2026-08-14 14:04:53 +0300

    text

--- src/data/ServiceDatas.js
+++ src/data/ServiceDatas.js
@@ -1,23 +1,28 @@
-// src/data/servicesData.js
-import { FaDesktop, FaPaintBrush , FaPencilRuler} from "react-icons/fa";
+import { FaCode, FaFileAlt, FaKeyboard, FaMapMarkedAlt } from "react-icons/fa";
 
 export const servicesData = [
   {
     id: 1,
-    title: "Développement Web",
-    description: "Création d’interfaces web modernes, performantes et accessibles.",
-    icon: FaDesktop,
+    title: "Développement Front-End",
+    description: "Création d'interfaces web modernes, performantes et accessibles.",
+    icon: FaCode,
   },
   {
     id: 2,
-    title: "Graphic Design",
-    description: "Création d'outils visuels sublimes pour une meilleure attraction ",
-    icon: FaPencilRuler,
+    title: "Transcription",
+    description: "Transcription précise de contenus audio et vidéo en texte structuré.",
+    icon: FaFileAlt,
   },
   {
     id: 3,
-    title: "Audio Transcripts",
-    description: "Transcript audio datas into text ",
-    icon: FaPaintBrush,
+    title: "Collecte de données sur terrain",
+    description: "Collecte rigoureuse d'informations sur le terrain, selon vos besoins.",
+    icon: FaMapMarkedAlt,
+  },
+  {
+    id: 4,
+    title: "Data Entry",
+    description: "Saisie, vérification et organisation de vos données avec précision.",
+    icon: FaKeyboard,
   },
 ];
