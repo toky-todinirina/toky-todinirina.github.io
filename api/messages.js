16b85c0fee724baa7affdebc3c48b2817de9b346
toky-todinirina <tokyangelo050@gmail.com>
2026-08-16 13:49:46 +0300

    ajour des réponse automatique aux messages reçus

--- api/messages.js
+++ api/messages.js
@@ -1,6 +1,10 @@
 import connectToDatabase from "./lib/mongodb.js";
 import Message from "./models/Message.js";
+import {Resend} from "resend"; 
 
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 export default async function handler(req, res) {
   // Autoriser uniquement POST
   if (req.method !== "POST") {
@@ -38,6 +42,49 @@ export default async function handler(req, res) {
       subject: subject?.trim() || "",
       message: message.trim(),
     });
+
+
+    await resend.emails.send({
+  from: "Portfolio <onboarding@resend.dev>",
+  to: [email.trim()],
+  subject: "Merci pour votre message",
+  html: `
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+      <h2>Bonjour ${name.trim()},</h2>
+
+      <p>
+        Merci d'avoir pris le temps de me contacter via mon portfolio.
+      </p>
+
+      <p>
+        J'ai bien reçu votre message et je reviendrai vers vous
+        dans les meilleurs délais.
+      </p>
+
+      <hr />
+
+      <p>
+        <strong>Objet :</strong>
+        ${subject?.trim() || "Non précisé"}
+      </p>
+
+      <p>
+        <strong>Votre message :</strong>
+      </p>
+
+      <p>
+        ${message.trim()}
+      </p>
+
+      <hr />
+
+      <p>
+        Cordialement,<br />
+        <strong>Toky Todinirina</strong>
+      </p>
+    </div>
+  `,
+});
 
     return res.status(201).json({
       success: true,
